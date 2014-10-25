640701513b854e2d0c215939cd7d87812f57df67
youzhang <youzhang@shu.edu.com>
2014-10-25 21:13:55 +0800

    1.  add comments to the config

--- ResultServer/Lib/Config.py
+++ ResultServer/Lib/Config.py
@@ -19,15 +19,19 @@ class testConfig(object):
         configFile = sysConfigFile()
         ip = configFile.getSysConfigItem("Misc","Address")
         port = int(configFile.getSysConfigItem("Misc","Port"))
+        # ip address config
         self.address = (ip,port)
+        # folder config
         self.resultFolder = configFile.getSysConfigItem("Misc","resultFolder")
         self.resultFile = os.path.join(self.resultFolder,common.localTime+".xlsx")
         self.rawDataStoreFolder = configFile.getSysConfigItem("Misc","rawDataFolder")
         self.rawDataFilePath = os.path.join(self.rawDataStoreFolder,"test.lvm")
+        self.LogFilePath = configFile.getSysConfigItem("Misc","LogPath")
+        # start Button & end Button config
         self.startButtonPos = tuple(configFile.getSysConfigItem("Misc","startButtonPos").split())
         self.stopButtonPos = tuple(configFile.getSysConfigItem("Misc","stopButtonPos").split())
+        # diagram config
         self.initRow = tuple(configFile.getSysConfigItem("Misc","initRow").split())
         self.chartType = configFile.getSysConfigItem("Misc","chartType")
         self.chartStyle = int(configFile.getSysConfigItem("Misc","chartStyle"))
-        self.LogFilePath = configFile.getSysConfigItem("Misc","LogPath")
         self.targetDataPos = configFile.getSysConfigItem("Misc","targetDataPos")
