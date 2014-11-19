64077fb2a3059932ef6f8e6ea688deaad152ad5b
youzhang <youzhang@shu.edu.com>
2014-11-19 20:04:55 +0800

    add some sample configuration for app

--- TestClient/TestSuite.py
+++ TestClient/TestSuite.py
@@ -4,7 +4,12 @@ sys.path.append("Lib_2.0")
 from testClient import testClient
 from Config import testConfig
 
-
+# mode = {
+# vp9MvPerfTestCfg : "VP9MvFreePlayback",
+# vp9ChromeTestCfg : "VP9FixedChrome",
+# vp9FixedTestCfg : "VP9FixedPlayback",
+# hevcFixedTestCfg : "HEVCFixedPlayback"
+# }
 
 def init():
     cmd = "del Perf*"
@@ -13,6 +18,6 @@ def init():
 
 if __name__ == "__main__":
     init()
-    myClientTestCfg = testConfig()
-    myTestClient = testClient(myClientTestCfg,"HEVCFixedPlayback")
+    myClientTestCfg = testConfig()    
+    myTestClient = testClient(myClientTestCfg,"VP9MvFreePlayback")
     myTestClient.run()
