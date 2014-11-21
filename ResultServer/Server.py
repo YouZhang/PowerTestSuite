6b6e7e1bb018adfda48876525039a10121415da5
youzhang <youzhang@shu.edu.com>
2014-11-21 22:23:11 +0800

    fixed the issue that can not back up the raw data

--- ResultServer/Server.py
+++ ResultServer/Server.py
@@ -4,7 +4,10 @@ import sys
 sys.path.append('Lib')
 from PowerProcessor import *
 from Config import testConfig
+from common import localTime
 
+
+rawDataBackupFolder = os.path.join(myConfig.rawDataStoreFolder,localTime)
 
 
 def initEnv():
@@ -13,13 +16,14 @@ def initEnv():
     mkResFolderCMD = "mkdir " + myConfig.resultFolder
     mkRawDataFolderCMD = "mkdir " + myConfig.rawDataStoreFolder
     mkLogFolderCMD = "mkdir " + myConfig.LogFilePath
+    mkBackupFolderCMD = "mkdir " + rawDataBackupFolder
     os.system(mkResFolderCMD)
     os.system(mkRawDataFolderCMD)
     os.system(mkLogFolderCMD)
 
 def postProcess():
     common.appendLog("backup the raw lvm data...")
-    mvLVMFileCMD = 'copy RawData\*.lvm %s' % myConfig.rawDataStoreFolder
+    mvLVMFileCMD = 'copy RawData\*.lvm %s' % rawDataBackupFolder
     delLVMFileCMD = 'del RawData\*.lvm'
     os.system(mvLVMFileCMD)
     os.system(delLVMFileCMD)
