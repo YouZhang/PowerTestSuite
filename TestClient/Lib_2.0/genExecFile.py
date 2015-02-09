36184f59b3ae1639eb39807ed8b7334b018bb234
youzhang <youzhang@shu.edu.com>
2015-02-09 10:45:55 +0800

    Binary generate script for all the scripts

--- TestClient/Lib_2.0/genExecFile.py
+++ TestClient/Lib_2.0/genExecFile.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import py2exe
 import sys
 
-script = ['..\localCalculate.py','..\PnPTestSuite_Beta1_0.py','downloadDriver.py','cpuMonitor.py','..\setup.py']
+script = ['..\localCalculate.py','..\PnPTestSuite_Beta1_0.py','downloadDriver.py','..\enableApp_InternalOnly.py']
 
 options = {"py2exe":
 
@@ -11,8 +11,10 @@ options = {"py2exe":
      "optimize": 2,     
      "bundle_files": 1 }
     }
-setup(
-    options = options,
-    zipfile=None,
-    console=[{"script": "%s" % script[2] }]
-    )
+
+for i in range( len(script) ):
+    setup(
+        options = options,
+        zipfile=None,
+        console=[{"script": "%s" % script[i] }]
+        )
